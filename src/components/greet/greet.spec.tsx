e56ffbba50ac1ad0597e3f0e7e132725137817b1
Pavan Mundada <pavanpmundada5793@gmail.com>
2023-04-12 16:32:48 +0530

    replaced test with {it} & used different method to display describe block

--- src/components/greet/greet.spec.tsx
+++ src/components/greet/greet.spec.tsx
@@ -3,15 +3,16 @@ import { render, screen } from "@testing-library/react";
 import { Greet } from "./greet";
 
 describe("Greet", () => {
-  test("renders correctly", () => {
-    //test.skip this test for running while other test cases should run.
-    render(<Greet />); // render api will give us the copy of virtual Dom
-    const textElement = screen.getByText(/hello/i); // it will ignore the case sensitiveness and will accept both lowercase & uppercase.
-    expect(textElement).toBeInTheDocument(); // expect & test are provided by jest
+  it("renders correctly", () => {
+    // we can replace test with it
+    render(<Greet />);
+    const textElement = screen.getByText(/hello/i);
+    expect(textElement).toBeInTheDocument();
   });
+});
 
-  test("renders with correct name", () => {
-    // test.only will run only this current test
+describe("Greet gracefully", () => {
+  it("renders with correct name", () => {
     render(<Greet name="Pavan" />);
     const textElement = screen.getByText("Hello Pavan");
     expect(textElement).toBeInTheDocument();
