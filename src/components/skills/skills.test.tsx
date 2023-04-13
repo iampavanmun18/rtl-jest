fc379022b3d84b532ddc0272be93cbceb09b307d
Pavan Mundada <pavanpmundada5793@gmail.com>
2023-04-13 17:38:47 +0530

    findByRole for checking Start Learning Button rendered

--- src/components/skills/skills.test.tsx
+++ src/components/skills/skills.test.tsx
@@ -1,39 +1,52 @@
 import { render, screen } from "@testing-library/react";
 import { Skills } from "./skills";
 
-
 //getAllByRole used to find multiple elements in the dom
-describe('Skills', () => {
-    const skills = ['html', 'JS', 'React']
+describe("Skills", () => {
+  const skills = ["html", "JS", "React"];
 
-    test('should render Skills', () => {
-        render(<Skills skills={skills}/>)
-        const listElement = screen.getByRole('list')
-        expect(listElement).toBeInTheDocument();
-     })
+  test("should render Skills", () => {
+    render(<Skills skills={skills} />);
+    const listElement = screen.getByRole("list");
+    expect(listElement).toBeInTheDocument();
+  });
 
-     test('render a list of skills', () => {
-        render(<Skills skills={skills}/>)
-        const listItems = screen.getAllByRole('listitem');
-        console.log("listItems",listItems);
-        expect(listItems).toHaveLength(skills.length)
-      })
+  test("render a list of skills", () => {
+    render(<Skills skills={skills} />);
+    const listItems = screen.getAllByRole("listitem");
+    console.log("listItems", listItems);
+    expect(listItems).toHaveLength(skills.length);
+  });
 
-      test('render Login Button', () => {
-        render(<Skills skills={skills}/>);
-        const loginButton = screen.getByRole('button',{
-          name:'Login'
-        })
-        expect(loginButton).toBeInTheDocument();
-      })
+  test("render Login Button", () => {
+    render(<Skills skills={skills} />);
+    const loginButton = screen.getByRole("button", {
+      name: "Login",
+    });
+    expect(loginButton).toBeInTheDocument();
+  });
 
-        //queryByRole is imp for asserting an element that is not present.Also returns the matching node for a query and return null if no elements matches.
-      test('Start Learning button is not renderred', () => {
-        render(<Skills skills={skills}/>);
-        const startLearningButton = screen.queryByRole('button',{
-          name:'Start Learning',
-        });
-        expect(startLearningButton).not.toBeInTheDocument();
-      })
+  //queryByRole is imp for asserting an element that is not present.Also returns the matching node for a query and return null if no elements matches.
+  test("Start Learning button is not renderred", () => {
+    render(<Skills skills={skills} />);
+    const startLearningButton = screen.queryByRole("button", {
+      name: "Start Learning",
+    });
+    // expect(startLearningButton).not.toBeInTheDocument();
+    expect(startLearningButton).toBeNull(); //Since queryByRole returns null where button does not found
+  });
 
-})
+  test("Start Learning button is displayed eventually", async () => {
+    render(<Skills skills={skills} />);
+    const startLearningButton = await screen.findByRole(
+      "button",
+      {
+        name: "Start Learning",
+      },
+      {
+        timeout: 2000,
+      }
+    );
+    expect(startLearningButton).toBeInTheDocument();
+  });
+});
--- src/components/skills/skills.tsx
+++ src/components/skills/skills.tsx
@@ -1,10 +1,17 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { SkillProps } from './skills.types'
 
 export const Skills = (props: SkillProps) => {
     const [isLoggedIn, setisLoggedIn] = useState(false)
 
     const {skills} = props
+
+    useEffect(() => {
+      setTimeout(() => {
+        setisLoggedIn(true)
+      }, 1000);
+    },[])
+
   return (
     <>
     <ul>
