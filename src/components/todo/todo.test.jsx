59d021659637d2f83802bf9c50b1f39e1d665aec
Pavan Mundada <pavanpmundada5793@gmail.com>
2023-04-14 14:48:00 +0530

    updated todo test case with help of react-test-renderer

--- src/components/todo/todo.test.jsx
+++ src/components/todo/todo.test.jsx
@@ -2,26 +2,26 @@ import { render, screen, cleanup } from "@testing-library/react";
 import renderer from "react-test-renderer";
 import { Todo } from "./todo";
 
-describe("test todo", () => {
-  test("should render non-completed Todo", () => {
-    const todo = { id: 1, title: "wash dishes", completed: false };
-    render(<Todo todo={todo} />);
-    // const textElement = screen.getByText("Hello from Todo!");
-    const todoElement = screen.getByTestId("todo-1");
-    expect(todoElement).toBeInTheDocument();
-    expect(todoElement).toHaveTextContent("wash dishes");
-    expect(todoElement).not.toContainHTML("<strike>");
-  });
+// describe("test todo", () => {
+//   test("should render non-completed Todo", () => {
+//     const todo = { id: 1, title: "wash dishes", completed: false };
+//     render(<Todo todo={todo} />);
+//     // const textElement = screen.getByText("Hello from Todo!");
+//     const todoElement = screen.getByTestId("todo-1");
+//     expect(todoElement).toBeInTheDocument();
+//     expect(todoElement).toHaveTextContent("wash dishes");
+//     expect(todoElement).not.toContainHTML("<strike>");
+//   });
 
-  test("should render completed Todo", () => {
-    const todo = { id: 2, title: "wash car", completed: true };
-    render(<Todo todo={todo} />);
-    // const textElement = screen.getByText("Hello from Todo!");
-    const todoElement = screen.getByTestId("todo-2");
-    expect(todoElement).toBeInTheDocument();
-    expect(todoElement).toHaveTextContent("wash car");
-  });
-});
+//   test("should render completed Todo", () => {
+//     const todo = { id: 2, title: "wash car", completed: true };
+//     render(<Todo todo={todo} />);
+//     // const textElement = screen.getByText("Hello from Todo!");
+//     const todoElement = screen.getByTestId("todo-2");
+//     expect(todoElement).toBeInTheDocument();
+//     expect(todoElement).toHaveTextContent("wash car");
+//   });
+// });
 
 describe("should render snapshot using react-test-renderer create method", () => {
   test("matches snapshot", () => {
